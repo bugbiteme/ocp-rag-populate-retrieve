dfc541047696e460dbdc66040ec923ea73ad8ea6
bugbiteme <leon.s.levy@gmail.com>
2025-07-28 14:42:16 -0700

    code

--- vector_loader.py
+++ vector_loader.py
@@ -1,11 +1,8 @@
-import os
-os.environ["CHROMA_USE_EMBEDDING_DB"] = "false"
+import chromadb
+from chromadb.config import Settings
 
 def load_to_chroma(chunks):
-    from chromadb.client import HttpClient
-    from chromadb.config import Settings
-
-    client = HttpClient(Settings(
+    client = chromadb.Client(Settings(
         chroma_api_impl="rest",
         chroma_server_host="chroma.player1.svc.cluster.local",
         chroma_server_http_port=8080,
