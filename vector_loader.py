0c900e2805e0cc9bc6876b34590add7c5176ab0a
bugbiteme <leon.s.levy@gmail.com>
2025-07-28 16:01:48 -0700

    code

--- vector_loader.py
+++ vector_loader.py
@@ -3,6 +3,7 @@ from chromadb.config import Settings
 
 def load_to_chroma(chunks):
     client = Client(Settings(
+        chroma_api_impl="chromadb.api.fastapi.FastAPIClient",
         chroma_server_host="chroma.player1.svc.cluster.local",
         chroma_server_http_port=8080,
     ))
